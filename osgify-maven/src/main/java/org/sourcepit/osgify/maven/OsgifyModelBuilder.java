edd3fb5f512598cae01a1f45b3dc13f783851469
Bernd Vogt <bernd.vogt@sourcepit.org>
2013-05-13 15:22:24 +0200

    [refactoring] Migrating to maven-dependency-model

---
+++ osgify-maven/src/main/java/org/sourcepit/osgify/maven/OsgifyModelBuilder.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2013 Sourcepit.org contributors and others. All rights reserved. This program and the accompanying
+ * materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.sourcepit.osgify.maven;
+
+import static org.sourcepit.common.utils.lang.Exceptions.pipe;
+
+import java.util.Collection;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.apache.maven.model.Dependency;
+import org.apache.maven.project.DependencyResolutionException;
+import org.apache.maven.project.ProjectBuildingException;
+import org.sourcepit.maven.dependency.model.DependencyModel;
+import org.sourcepit.maven.dependency.model.DependencyModelResolver;
+import org.sourcepit.osgify.core.model.context.OsgifyContext;
+import org.sourcepit.osgify.maven.impl.OsgifyStubModelCreator;
+
+@Named
+public class OsgifyModelBuilder
+{
+   @Inject
+   private DependencyModelResolver dependencyModelResolver;
+   
+   @Inject
+   private OsgifyStubModelCreator stubModelCreator;
+
+   private OsgifyContext build(Collection<Dependency> dependencies)
+   {
+      final DependencyModel dependencyModel;
+
+      try
+      {
+         dependencyModel = dependencyModelResolver.resolve(dependencies);
+      }
+      catch (ProjectBuildingException e)
+      {
+         throw pipe(e);
+      }
+      catch (DependencyResolutionException e)
+      {
+         throw pipe(e);
+      }
+
+      final OsgifyContext osgifyModel = stubModelCreator.create(dependencyModel);
+      // resolve sources
+      // scan contents
+      // derive manifests
+
+      return null;
+   }
+
+
+   public static class Request
+   {
+      private boolean includeSource;
+   }
+}
---
+++ osgify-maven/src/main/java/org/sourcepit/osgify/maven/impl/OsgifyStubModelCreator.java
@@ -0,0 +1,93 @@
+/**
+ * Copyright (c) 2013 Sourcepit.org contributors and others. All rights reserved. This program and the accompanying
+ * materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.sourcepit.osgify.maven.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.inject.Named;
+
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.sourcepit.common.maven.model.MavenArtifact;
+import org.sourcepit.common.maven.model.MavenDependency;
+import org.sourcepit.common.maven.model.MavenModelFactory;
+import org.sourcepit.maven.dependency.model.DependencyModel;
+import org.sourcepit.maven.dependency.model.DependencyNode;
+import org.sourcepit.maven.dependency.model.DependencyTree;
+import org.sourcepit.osgify.core.model.context.BundleCandidate;
+import org.sourcepit.osgify.core.model.context.BundleReference;
+import org.sourcepit.osgify.core.model.context.ContextModelFactory;
+import org.sourcepit.osgify.core.model.context.OsgifyContext;
+
+@Named
+public class OsgifyStubModelCreator
+{
+   public OsgifyContext create(DependencyModel dependencyModel)
+   {
+      final Map<MavenArtifact, BundleCandidate> artifactToBundle = new HashMap<MavenArtifact, BundleCandidate>();
+      for (MavenArtifact artifact : dependencyModel.getArtifacts())
+      {
+         BundleCandidate bundle = toBundleCandidate(artifact);
+         artifactToBundle.put(artifact, bundle);
+      }
+
+      final OsgifyContext osgifyModel = ContextModelFactory.eINSTANCE.createOsgifyContext();
+      for (DependencyTree dependencyTree : dependencyModel.getDependencyTrees())
+      {
+         final BundleCandidate bundle = artifactToBundle.get(dependencyTree.getArtifact());
+         osgifyModel.getBundles().add(bundle);
+         for (DependencyNode dependencyNode : dependencyTree.getDependencyNodes())
+         {
+            addBundleReferences(artifactToBundle, bundle, dependencyNode);
+         }
+      }
+      return osgifyModel;
+   }
+
+   private void addBundleReferences(Map<MavenArtifact, BundleCandidate> artifactToBundle, BundleCandidate bundle,
+      DependencyNode dependencyNode)
+   {
+      if (dependencyNode.isSelected())
+      {
+         final BundleReference reference = toBundleReference(artifactToBundle, dependencyNode);
+         bundle.getDependencies().add(reference);
+
+         for (DependencyNode childNode : dependencyNode.getChildren())
+         {
+            addBundleReferences(artifactToBundle, bundle, childNode);
+         }
+      }
+   }
+
+   private BundleReference toBundleReference(Map<MavenArtifact, BundleCandidate> artifactToBundle,
+      DependencyNode dependencyNode)
+   {
+      final BundleReference reference = ContextModelFactory.eINSTANCE.createBundleReference();
+      reference.setTarget(artifactToBundle.get(dependencyNode.getArtifact()));
+
+      final MavenDependency dependency = MavenModelFactory.eINSTANCE.createMavenDependency();
+      dependency.setGroupId(dependencyNode.getGroupId());
+      dependency.setArtifactId(dependencyNode.getArtifactId());
+      dependency.setVersionConstraint(dependencyNode.getEffectiveVersionConstraint());
+      dependency.setType(dependencyNode.getType());
+      dependency.setClassifier(dependencyNode.getClassifier());
+      dependency.setScope(dependencyNode.getEffectiveScope());
+      dependency.setOptional(dependencyNode.isOptional());
+
+      reference.addExtension(dependency);
+
+      return reference;
+   }
+
+   private BundleCandidate toBundleCandidate(MavenArtifact artifact)
+   {
+      final BundleCandidate bundle = ContextModelFactory.eINSTANCE.createBundleCandidate();
+      bundle.setLocation(artifact.getFile());
+      bundle.addExtension(EcoreUtil.copy(artifact));
+      return bundle;
+   }
+}
