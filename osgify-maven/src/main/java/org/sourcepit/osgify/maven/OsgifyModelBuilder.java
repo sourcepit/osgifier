3cd47b98f333d5bdc5c6bd74bafd17129d101bc0
Bernd Vogt <bernd.vogt@sourcepit.org>
2013-05-23 15:12:51 +0200

    [improvement] Logging

--- osgify-maven/src/main/java/org/sourcepit/osgify/maven/OsgifyModelBuilder.java
+++ osgify-maven/src/main/java/org/sourcepit/osgify/maven/OsgifyModelBuilder.java
@@ -12,15 +12,18 @@ import static org.sourcepit.common.utils.io.IO.read;
 import static org.sourcepit.common.utils.lang.Exceptions.pipe;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.TimeZone;
 
 import javax.inject.Inject;
@@ -33,6 +36,7 @@ import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.slf4j.Logger;
 import org.sourcepit.common.manifest.Manifest;
 import org.sourcepit.common.manifest.osgi.BundleManifest;
 import org.sourcepit.common.manifest.osgi.BundleManifestFactory;
@@ -67,11 +71,15 @@ import org.sourcepit.osgify.core.resolve.SymbolicNameResolver;
 import org.sourcepit.osgify.core.resolve.VersionRangeResolver;
 import org.sourcepit.osgify.core.resolve.VersionResolver;
 import org.sourcepit.osgify.core.util.OsgifyContextUtils;
+import org.sourcepit.osgify.core.util.OsgifyContextUtils.BuildOrder;
 import org.sourcepit.osgify.maven.impl.OsgifyStubModelCreator;
 
 @Named
 public class OsgifyModelBuilder
 {
+   @Inject
+   private Logger log;
+
    @Inject
    private DependencyModelResolver dependencyModelResolver;
 
@@ -89,14 +97,25 @@ public class OsgifyModelBuilder
    {
       options = getOptions(options, timestamp);
 
+      log.info("Resolving bundle candidates...");
       final DependencyModel dependencyModel = resolve(dependencies);
+      log.info("------------------------------------------------------------------------");
 
       final OsgifyContext osgifyModel = createStubModel(dependencyModel);
-      applyNativeBundles(osgifyModel);
-      applyBuildOrder(osgifyModel);
-      applyJavaContent(generatorFilter, osgifyModel);
-      applyManifests(generatorFilter, options, osgifyModel);
 
+      log.info("Analyzing Java contents...");
+      applyNativeBundles(osgifyModel);
+      applyJavaContent(generatorFilter, osgifyModel);
+      log.info("------------------------------------------------------------------------");
+
+      log.info("Generating OSGi metadata...");
+      log.info("");
+      applySymbolicNameAndVersion(generatorFilter, osgifyModel, options);
+      log.info("Bundeling order:");
+      BuildOrder buildOrder = applyBuildOrder(osgifyModel);
+      logBuildOrder(buildOrder);
+      applyManifests(generatorFilter, options, osgifyModel);
+      log.info("------------------------------------------------------------------------");
       final Result result = new Result();
       result.dependencyModel = dependencyModel;
       result.osgifyModel = osgifyModel;
@@ -122,17 +141,42 @@ public class OsgifyModelBuilder
       };
    }
 
-   private void applyBuildOrder(final OsgifyContext osgifyModel)
+   private BuildOrder applyBuildOrder(final OsgifyContext osgifyModel)
    {
       final EList<BundleCandidate> bundles = osgifyModel.getBundles();
-      final List<BundleCandidate> orderedBundles = OsgifyContextUtils.computeBuildOrder(osgifyModel)
-         .getOrderedBundles();
+      final BuildOrder buildOrder = OsgifyContextUtils.computeBuildOrder(osgifyModel);
+      final List<BundleCandidate> orderedBundles = buildOrder.getOrderedBundles();
       for (int i = 0; i < orderedBundles.size(); i++)
       {
          bundles.move(i, orderedBundles.get(i));
       }
+      return buildOrder;
    }
 
+   private void logBuildOrder(BuildOrder buildOrder)
+   {
+      final Set<BundleCandidate> cyclicBundles = new HashSet<BundleCandidate>();
+      for (List<BundleCandidate> cycle : buildOrder.getCycles())
+      {
+         cyclicBundles.addAll(cycle);
+      }
+      for (BundleCandidate bundle : buildOrder.getOrderedBundles())
+      {
+         if (cyclicBundles.contains(bundle))
+         {
+            log.warn("* {} @ {} (cyclic requirements detected)", getName(bundle), getBundleKey(bundle));
+         }
+         else
+         {
+            log.info("* {} / {}", getName(bundle), getBundleKey(bundle));
+         }
+      }
+   }
+
+   private String getName(BundleCandidate bundle)
+   {
+      return bundle.getExtension(MavenArtifact.class).getArtifactKey().toString();
+   }
 
    private DependencyModel resolve(Collection<Dependency> dependencies)
    {
@@ -220,54 +264,12 @@ public class OsgifyModelBuilder
    private void applyManifests(ManifestGeneratorFilter generatorFilter, PropertiesSource options,
       OsgifyContext osgifyModel)
    {
-      Map<String, BundleCandidate> keyToBundle = new HashMap<String, BundleCandidate>();
-
-      for (BundleCandidate bundle : osgifyModel.getBundles())
-      {
-         if (generatorFilter.isGenerateManifest(bundle) && !generatorFilter.isSourceBundle(bundle))
-         {
-            final BundleManifest manifest = BundleManifestFactory.eINSTANCE.createBundleManifest();
-            bundle.setManifest(manifest);
-
-            final String symbolicName = symbolicNameResolver.resolveSymbolicName(bundle);
-            checkState(symbolicName != null, "Failed to determine bundle symbolic name for %s", bundle.getLocation());
-            manifest.setBundleSymbolicName(symbolicName);
-            bundle.setSymbolicName(symbolicName);
-
-            final Version version = versionResolver.resolveVersion(bundle, options);
-            checkState(version != null, "Failed to determine bundle version for %s", bundle.getLocation());
-            manifest.setBundleVersion(version);
-            bundle.setVersion(version);
-
-            final String bundleKey = getBundleKey(bundle);
-
-            final BundleCandidate conflictBundle = keyToBundle.get(bundleKey);
-            if (conflictBundle == null)
-            {
-               keyToBundle.put(bundleKey, bundle);
-            }
-            else
-            {
-               final List<String> conflictNames = symbolicNameResolver.resolveSymbolicNames(conflictBundle);
-               final List<String> names = symbolicNameResolver.resolveSymbolicNames(bundle);
-               if (nameConflictResolver.resolveNameConflict(conflictBundle, conflictNames, bundle, names))
-               {
-                  keyToBundle.remove(bundleKey);
-                  keyToBundle.put(getBundleKey(conflictBundle), conflictBundle);
-                  keyToBundle.put(getBundleKey(bundle), bundle);
-               }
-               else
-               {
-                  // TODO panic!
-               }
-            }
-         }
-      }
-
       for (BundleCandidate bundle : osgifyModel.getBundles())
       {
          if (generatorFilter.isGenerateManifest(bundle))
          {
+            log.info("Bundeling {} / {}", getName(bundle), getBundleKey(bundle));
+
             for (BundleReference reference : bundle.getDependencies())
             {
                // TODO move elsewhere
@@ -282,7 +284,11 @@ public class OsgifyModelBuilder
 
             if (generatorFilter.isSourceBundle(bundle))
             {
-               applySourceBundles(bundle);
+               final BundleCandidate targetBundle = bundle.getTargetBundle();
+               final Version version = targetBundle.getVersion();
+               final BundleManifest manifest = bundle.getManifest();
+               manifest.setHeader("Eclipse-SourceBundle", targetBundle.getSymbolicName() + ";version=\"" + version
+                  + "\";roots:=\".\"");
             }
             else
             {
@@ -292,6 +298,81 @@ public class OsgifyModelBuilder
                dynamicImports.append(bundle);
             }
          }
+         else
+         {
+            log.info("Skipped {} / {}", getName(bundle), getBundleKey(bundle));
+         }
+      }
+   }
+
+   private void applySymbolicNameAndVersion(ManifestGeneratorFilter generatorFilter, OsgifyContext osgifyModel,
+      PropertiesSource options)
+   {
+      final Map<String, BundleCandidate> keyToBundle = new HashMap<String, BundleCandidate>();
+      final List<BundleCandidate> sourceBundles = new ArrayList<BundleCandidate>();
+      for (BundleCandidate bundle : osgifyModel.getBundles())
+      {
+         if (generatorFilter.isGenerateManifest(bundle))
+         {
+            if (generatorFilter.isSourceBundle(bundle))
+            {
+               sourceBundles.add(bundle);
+            }
+            else
+            {
+               final BundleManifest manifest = BundleManifestFactory.eINSTANCE.createBundleManifest();
+               bundle.setManifest(manifest);
+
+               final String symbolicName = symbolicNameResolver.resolveSymbolicName(bundle);
+               checkState(symbolicName != null, "Failed to determine bundle symbolic name for %s", bundle.getLocation());
+               manifest.setBundleSymbolicName(symbolicName);
+               bundle.setSymbolicName(symbolicName);
+
+               final Version version = versionResolver.resolveVersion(bundle, options);
+               checkState(version != null, "Failed to determine bundle version for %s", bundle.getLocation());
+               manifest.setBundleVersion(version);
+               bundle.setVersion(version);
+
+               final String bundleKey = getBundleKey(bundle);
+
+               final BundleCandidate conflictBundle = keyToBundle.get(bundleKey);
+               if (conflictBundle == null)
+               {
+                  keyToBundle.put(bundleKey, bundle);
+               }
+               else
+               {
+                  final List<String> conflictNames = symbolicNameResolver.resolveSymbolicNames(conflictBundle);
+                  final List<String> names = symbolicNameResolver.resolveSymbolicNames(bundle);
+                  if (nameConflictResolver.resolveNameConflict(conflictBundle, conflictNames, bundle, names))
+                  {
+                     keyToBundle.remove(bundleKey);
+                     keyToBundle.put(getBundleKey(conflictBundle), conflictBundle);
+                     keyToBundle.put(getBundleKey(bundle), bundle);
+                  }
+                  else
+                  {
+                     // TODO panic!
+                  }
+               }
+            }
+         }
+      }
+
+      for (BundleCandidate bundle : sourceBundles)
+      {
+         final BundleCandidate targetBundle = bundle.getTargetBundle();
+         final String symbolicName = targetBundle.getSymbolicName();
+         final Version version = targetBundle.getVersion();
+
+         final BundleManifest manifest = BundleManifestFactory.eINSTANCE.createBundleManifest();
+         bundle.setManifest(manifest);
+
+         manifest.getBundleSymbolicName(true).setSymbolicName(symbolicName + ".source");
+         bundle.setSymbolicName(symbolicName + ".source");
+
+         manifest.setBundleVersion(version);
+         bundle.setVersion(version);
       }
    }
 
@@ -300,28 +381,9 @@ public class OsgifyModelBuilder
       return bundle.getSymbolicName() + "_" + bundle.getVersion().toString();
    }
 
-   private void applySourceBundles(BundleCandidate bundle)
+   private List<BundleCandidate> applyNativeBundles(final OsgifyContext osgifyModel)
    {
-      final BundleCandidate targetBundle = bundle.getTargetBundle();
-      final String symbolicName = targetBundle.getSymbolicName();
-      final Version version = targetBundle.getVersion();
-
-      final BundleManifest manifest = BundleManifestFactory.eINSTANCE.createBundleManifest();
-      bundle.setManifest(manifest);
-
-      manifest.getBundleSymbolicName(true).setSymbolicName(symbolicName + ".source");
-      bundle.setSymbolicName(symbolicName + ".source");
-
-      manifest.setBundleVersion(version);
-      bundle.setVersion(version);
-
-      // Eclipse-SourceBundle: com.ibm.icu;version="4.4.2.v20110823";roots:="."
-      manifest.setHeader("Eclipse-SourceBundle", targetBundle.getSymbolicName() + ";version=\"" + version
-         + "\";roots:=\".\"");
-   }
-
-   private void applyNativeBundles(final OsgifyContext osgifyModel)
-   {
+      final List<BundleCandidate> nativeBundles = new ArrayList<BundleCandidate>();
       for (BundleCandidate bundle : osgifyModel.getBundles())
       {
          if (bundle.getLocation() != null)
@@ -349,15 +411,20 @@ public class OsgifyModelBuilder
                   // TODO deprecate! replace with operation
                   bundle.setSymbolicName(manifest.getBundleSymbolicName().getSymbolicName());
                   bundle.setVersion(manifest.getBundleVersion());
+
+                  nativeBundles.add(bundle);
                }
             }
             catch (PipedException e)
             {
-               // TODO log
-               e.printStackTrace();
+               if (e.adapt(FileNotFoundException.class) == null)
+               {
+                  throw e;
+               }
             }
          }
       }
+      return nativeBundles;
    }
 
    private OsgifyContext createStubModel(final DependencyModel dependencyModel)
